eba38f81f9eab74b997617b32700c3d6c1e4e3b2
Madanbekov Marsel <madanbekovmarsel@gmail.com>
2023-06-20 14:20:30 +0600

    main-user component

--- src/app/views/usersMainPages/main-user/main-user.component.ts
+++ src/app/views/usersMainPages/main-user/main-user.component.ts
@@ -1,22 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SidebarService} from "../../../service/sidebar.service";
 import {MatDialog, MatDialogRef} from "@angular/material/dialog";
 import {Router} from "@angular/router";
 import {CreateGroupComponent} from "../../dialogs/create-group/create-group.component";
+import {User} from "../../../models/User";
+import {UserService} from "../../../service/entityServices/user.service";
+import {LanguageService} from "../../../service/translations/language.service";
+import {TokenStorageService} from "../../../service/token-storage.service";
 
 @Component({
   selector: 'app-main-user',
   templateUrl: './main-user.component.html',
   styleUrls: ['./main-user.component.scss']
 })
-export class MainUserComponent {
+export class MainUserComponent implements OnInit{
+  currentLang!: string;
+  currentUser!: User;
   constructor(private sidebarService: SidebarService,
+              private userService: UserService,
+              private languageService: LanguageService,
+              private tokenService: TokenStorageService,
               private dialog: MatDialog,
               private router: Router) {
   }
 
   sidebarToggle() {
     this.sidebarService.toggle();
+  }
+  ngOnInit(): void {
+    this.languageService.lang$.subscribe(lang => {
+      this.currentLang = lang;
+    });
+    this.userService.getCurrentUser().subscribe(user =>{
+      this.currentUser = user;
+    })
   }
   groupDetails(){
     this.router.navigate(['group-details']);
@@ -37,4 +54,15 @@ export class MainUserComponent {
       }
     });
   }
+  language(lang:string){
+    this.languageService.toggle(lang);
+  }
+  profile() {
+    this.router.navigate(['user-details'],{queryParams: {userId:0}});
+  }
+  logout() {
+    this.tokenService.logOut();
+    this.router.navigate(['login'])
+  }
+
 }
